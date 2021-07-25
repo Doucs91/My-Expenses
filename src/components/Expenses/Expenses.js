a102080ec94d89007143ecf4fca0c3b94b419a76
Sady Doucoure <sadydoucoure@gmail.com>
2021-07-25 16:01:33 -0400

    Adding components

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,16 @@
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css"
+import Card from "../UI/Card"
+
+const Expenses = (props) =>{
+  return (
+    <Card className="expenses">
+      <ExpenseItem expense={props.items[0]} />
+      <ExpenseItem expense={props.items[1]} />
+      <ExpenseItem expense={props.items[2]} />
+      <ExpenseItem expense={props.items[3]} />
+    </Card>
+  );
+}
+
+export default Expenses;
